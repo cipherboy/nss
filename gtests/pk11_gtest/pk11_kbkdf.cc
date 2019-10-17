17722e14c69453ab094c12ad5277543d4691eaf6
Alexander Scheel <ascheel@redhat.com>
2019-10-17 07:13:14 -0400

    FIXUP gtests/pk11_gtest/pk11_kbkdf.cc
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

---
+++ gtests/pk11_gtest/pk11_kbkdf.cc
@@ -0,0 +1,64 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <memory>
+#include "nss.h"
+#include "pk11pub.h"
+#include "secerr.h"
+#include "sechash.h"
+
+#include "blapi.h"
+
+#include "gtest/gtest.h"
+#include "nss_scoped_ptrs.h"
+#include "util.h"
+
+#define DIM(a) (sizeof((a))/sizeof((a)[0]))
+
+namespace nss_test {
+class Pkcs11KbkdfTest : public ::testing::Test {
+ protected:
+  ScopedPK11SymKey ImportKey(CK_MECHANISM_TYPE mech, SECItem *key_item) {
+    ScopedPK11SlotInfo slot(PK11_GetInternalSlot());
+    if (!slot) {
+      ADD_FAILURE() << "Can't get slot";
+      return nullptr;
+    }
+
+    ScopedPK11SymKey result(PK11_ImportSymKey(
+        slot.get(), mech, PK11_OriginUnwrap, CKA_SIGN, key_item, nullptr));
+
+    return result;
+  }
+
+  void RunTest(void) {
+    uint8_t key[AES_128_KEY_LENGTH] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE,
+                                       0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88,
+                                       0x09, 0xCF, 0x4F, 0x3C};
+
+    SECItem key_item = {siBuffer, key, sizeof(key)/sizeof(key[0])};
+    ScopedPK11SymKey p11_key = ImportKey(CKM_AES_CMAC, &key_item);
+
+    CK_SP800_108_KDF_PARAMS kdfParams =
+    {
+      CKM_AES_CMAC,
+      0,
+      NULL,
+      0, /* no addition derived keys */
+      NULL     /* no addition derived keys */
+    };
+
+    SECItem kdfItem = { siBuffer, (unsigned char *)&kdfParams, sizeof(kdfParams) };
+
+    PK11SymKey *result = PK11_Derive(p11_key.get(), CKM_SP800_108_COUNTER_KDF, &kdfItem, CKM_AES_GCM, CKA_ENCRYPT, 0);
+    assert(result != NULL);
+  }
+};
+
+TEST_F(Pkcs11KbkdfTest, TestPkcs11v3SampleCounterModeKdf) {
+    RunTest();
+}
+}
